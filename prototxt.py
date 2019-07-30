a97dfa0064ca4762bf94cd45734041e4ada917ab
Faber <abrosua@gmail.com>
2019-07-30 10:08:28 +0700

    add "layers" option beside "layer" for reading the prototxt line with curly bracket

--- prototxt.py
+++ prototxt.py
@@ -4,6 +4,7 @@ from __future__ import division
 from collections import OrderedDict
 import caffe.proto.caffe_pb2 as caffe_pb2
 
+
 def parse_caffemodel(caffemodel):
     model = caffe_pb2.NetParameter()
     print('Loading caffemodel: ', caffemodel)
@@ -26,8 +27,8 @@ def parse_prototxt(protofile):
         line = fp.readline().strip()
         while line != '}':
             ltype = line_type(line)
-            if ltype == 0: # key: value
-                #print line
+            if ltype == 0:  # key: value
+                # print line
                 line = line.split('#')[0]
                 key, value = line.split(':')
                 key = key.strip()
@@ -39,7 +40,7 @@ def parse_prototxt(protofile):
                         block[key] = [block[key], value]
                 else:
                     block[key] = value
-            elif ltype == 1: # blockname {
+            elif ltype == 1:  # blockname {
                 key = line.split('{')[0].strip()
                 sub_block = parse_block(fp)
                 block[key] = sub_block
@@ -57,21 +58,24 @@ def parse_prototxt(protofile):
             line = fp.readline()
             continue
         ltype = line_type(line)
-        if ltype == 0: # key: value
+        if ltype == 0:  # key: value
             key, value = line.split(':')
             key = key.strip()
             value = value.strip().strip('"')
             # has_key was removed in Python 3
             if key in props:
-               if type(props[key]) == list:
-                   props[key].append(value)
-               else:
-                   props[key] = [props[key], value]
+                if type(props[key]) == list:
+                    props[key].append(value)
+                else:
+                    props[key] = [props[key], value]
             else:
                 props[key] = value
-        elif ltype == 1: # blockname {
+        elif ltype == 1:  # blockname {
             key = line.split('{')[0].strip()
             if key == 'layer':
+                layer = parse_block(fp)
+                layers.append(layer)
+            elif key == 'layers':
                 layer = parse_block(fp)
                 layers.append(layer)
             else:
@@ -86,6 +90,7 @@ def parse_prototxt(protofile):
     else:
         return props
 
+
 def is_number(s):
     try:
         float(s)
@@ -93,11 +98,12 @@ def is_number(s):
     except ValueError:
         return False
 
+
 def print_prototxt(net_info):
     # whether add double quote
     def format_value(value):
-        #str = u'%s' % value
-        #if str.isnumeric():
+        # str = u'%s' % value
+        # if str.isnumeric():
         if is_number(value):
             return value
         elif value == 'true' or value == 'false' or value == 'MAX' or value == 'SUM' or value == 'AVE':
@@ -106,18 +112,18 @@ def print_prototxt(net_info):
             return '\"%s\"' % value
 
     def print_block(block_info, prefix, indent):
-        blanks = ''.join([' ']*indent)
+        blanks = ''.join([' '] * indent)
         print('%s%s {' % (blanks, prefix))
-        for key,value in block_info.items():
+        for key, value in block_info.items():
             if type(value) == OrderedDict:
-                print_block(value, key, indent+4)
+                print_block(value, key, indent + 4)
             elif type(value) == list:
                 for v in value:
                     print('%s    %s: %s' % (blanks, key, format_value(v)))
             else:
                 print('%s    %s: %s' % (blanks, key, format_value(value)))
         print('%s}' % blanks)
-        
+
     props = net_info['props']
     layers = net_info['layers']
     print('name: \"%s\"' % props['name'])
@@ -130,12 +136,14 @@ def print_prototxt(net_info):
     for layer in layers:
         print_block(layer, 'layer', 0)
 
+
 def save_prototxt(net_info, protofile, region=True):
     fp = open(protofile, 'w')
+
     # whether add double quote
     def format_value(value):
-        #str = u'%s' % value
-        #if str.isnumeric():
+        # str = u'%s' % value
+        # if str.isnumeric():
         if is_number(value):
             return value
         elif value == 'true' or value == 'false' or value == 'MAX' or value == 'SUM' or value == 'AVE':
@@ -144,18 +152,18 @@ def save_prototxt(net_info, protofile, region=True):
             return '\"%s\"' % value
 
     def print_block(block_info, prefix, indent):
-        blanks = ''.join([' ']*indent)
-        print >>fp, '%s%s {' % (blanks, prefix)
-        for key,value in block_info.items():
+        blanks = ''.join([' '] * indent)
+        print >> fp, '%s%s {' % (blanks, prefix)
+        for key, value in block_info.items():
             if type(value) == OrderedDict:
-                print_block(value, key, indent+4)
+                print_block(value, key, indent + 4)
             elif type(value) == list:
                 for v in value:
                     print >> fp, '%s    %s: %s' % (blanks, key, format_value(v))
             else:
                 print >> fp, '%s    %s: %s' % (blanks, key, format_value(value))
         print >> fp, '%s}' % blanks
-        
+
     props = net_info['props']
     layers = net_info['layers']
     print >> fp, 'name: \"%s\"' % props['name']
@@ -173,6 +181,7 @@ def save_prototxt(net_info, protofile, region=True):
 
 if __name__ == '__main__':
     import sys
+
     if len(sys.argv) != 2:
         print('Usage: python prototxt.py model.prototxt')
         exit()
@@ -180,4 +189,3 @@ if __name__ == '__main__':
     net_info = parse_prototxt(sys.argv[1])
     print_prototxt(net_info)
     save_prototxt(net_info, 'tmp.prototxt')
-    
